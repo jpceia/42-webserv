5ef116951085a7ac774ace0603db3c96a65bac65
João Ceia <joao.p.ceia@gmail.com>
2022-03-03 22:13:47 +0000

    fixed getPath definition

--- src/HTTPRequest.cpp
+++ src/HTTPRequest.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   HTTPRequest.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
+/*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 15:33:45 by jceia             #+#    #+#             */
-/*   Updated: 2022/03/03 18:52:14 by jceia            ###   ########.fr       */
+/*   Updated: 2022/03/03 22:11:10 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -142,7 +142,7 @@ void HTTPRequest::addHeader(const std::string& key, const std::string& value)
     _headers[key] = value;
 }
 
-std::string getPath() const
+std::string HTTPRequest::getPath() const
 {
     return _path;
 }

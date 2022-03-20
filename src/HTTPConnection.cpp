b91e39c6a80038b27e84a594bf53792efc0e806d
João Ceia <joao.p.ceia@gmail.com>
2022-03-20 00:05:50 +0000

    reverted HTTPConnection::recvRequest logic

--- src/HTTPConnection.cpp
+++ src/HTTPConnection.cpp
@@ -6,7 +6,7 @@
 /*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 06:02:05 by jpceia            #+#    #+#             */
-/*   Updated: 2022/03/16 23:55:31 by jpceia           ###   ########.fr       */
+/*   Updated: 2022/03/19 22:08:28 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,15 +52,12 @@ HTTPRequest HTTPConnection::recvRequest() const
 {
     HTTPRequestParser request;
     ParseState state = PARSE_START;
-    size_t size = BUFF_SIZE;
 
     while (state != PARSE_COMPLETE)
     {
-        std::string chunk = TCPConnection::recv(size);
+        std::string chunk = TCPConnection::recv();
         state = request.parse(chunk);
-        if (chunk.size() < size && request.isByChunks() && state != PARSE_START)
-            state = PARSE_COMPLETE;
-        else if (chunk.empty())
+        if (chunk.empty())
             throw TCPConnection::DisconnectedException();
     }
     return request;

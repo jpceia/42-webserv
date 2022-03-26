2cb7be965ce6f5bbc4495788d96ad2e0dcd3366f
João Ceia <joao.p.ceia@gmail.com>
2022-03-26 03:00:01 +0000

    update formatedDate to GMT time

--- src/utils.cpp
+++ src/utils.cpp
@@ -6,7 +6,7 @@
 /*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 20:06:03 by jpceia            #+#    #+#             */
-/*   Updated: 2022/03/26 02:32:39 by jpceia           ###   ########.fr       */
+/*   Updated: 2022/03/26 02:59:45 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -132,11 +132,10 @@ std::string formatedDate(std::time_t rawtime)
     char buffer[80];
     struct tm * timeinfo;
 
-    timeinfo = std::localtime(&rawtime);
+    timeinfo = std::gmtime(&rawtime);
     strftime(buffer, 80, "%a, %d %b %Y %H:%M:%S %Z", timeinfo);
     return buffer;
 }
-
 
 std::time_t currentTime()
 {
